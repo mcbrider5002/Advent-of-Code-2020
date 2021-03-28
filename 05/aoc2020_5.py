a38600a635f791b4290481810af6a85b31734b32
unknown <mcbrider5002@gmail.com>
2021-03-28 18:00:20 +0100

    Replaced problem 5 solution with alternate solution, since it's better anyway...

--- 05/aoc2020_5.py
+++ 05/aoc2020_5.py
@@ -1,6 +1,3 @@
-with open("input.txt", "r") as f:
-    lines = [ln.strip() for ln in f if ln.strip() != ""]
-
 def seat_id(specifier):
     pre, post = specifier[:7], specifier[7:]
     def get_posn(sp): return sum(2 ** i if d else 0 for i, d in enumerate(reversed(sp)))
@@ -8,9 +5,7 @@ def seat_id(specifier):
     col_no = get_posn([ch == "R" for ch in post])
     return row_no * 8 + col_no
 
-def find_seat(specifiers):
-    ls = [True] * (128 * 8)
-    for specifier in specifiers: ls[seat_id(specifier)] = False
-    return ls.index(True, ls.index(False) + 1)
-
-print(f"Pt1: {max(seat_id(ln) for ln in lines)}\nPt2: {find_seat(lines)}")
+with open("input.txt", "r") as f:
+    numbers = set(seat_id(ln.strip()) for ln in f if ln.strip() != "")
+minm, maxm = min(numbers), max(numbers)
+print(f"Pt1: {maxm}\nPt2: {next(i for i in range(minm+1, maxm) if not i in numbers)}")
