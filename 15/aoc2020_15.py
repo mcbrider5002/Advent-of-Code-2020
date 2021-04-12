5fbeba47b8111d70e934d25647231ed510381826
unknown <mcbrider5002@gmail.com>
2021-04-12 08:37:25 +0100

    Slight stylistic cleanup to problem 15.

--- 15/aoc2020_15.py
+++ 15/aoc2020_15.py
@@ -1,11 +1,11 @@
-with open("input.txt", 'r') as f:
-    contents = f.readline().split(',')
-def solve(turns):
-    memory = {int(x) : i for i, x in enumerate(contents[:-1])}
-    n = int(contents[-1])
+def solve(contents, turns):
+    n, memory = int(contents[-1]), {int(x) : i for i, x in enumerate(contents[:-1])}
     for i in range(len(contents) - 1, turns - 1):
         new_n = i - memory[n] if n in memory else 0
         memory[n] = i
         n = new_n
     return n
-print(f"Pt1: {solve(2020)}\nPt2: {solve(30000000)}")
+    
+with open("input.txt", 'r') as f:
+    contents = f.readline().split(',')
+print(f"Pt1: {solve(contents, 2020)}\nPt2: {solve(contents, 30000000)}")
